817c583eb03c28e9fbb6435c989e9b37cabf21ca
Ishkaran Singh <ishkaran.singh@hotmail.com>
2015-05-23 01:05:28 +0530

    Adding Message/Packet structs

--- DarkNet/DarkNetStructs.cpp
+++ DarkNet/DarkNetStructs.cpp
@@ -1,4 +1,3 @@
-#include "DarkNetFuncs.h"
 #include "DarkNetStructs.h"
 
 namespace DarkNet
@@ -13,19 +12,49 @@ namespace DarkNet
 		CloseSocket(sd);
 	}
 
+	Address::Address()
+	{
+		memset(&addr, 0, sizeof(SockAddr));
+	}
+
 	Address::Address(int port_num)
 	{
-		CreateSockAddress(address, NULL, port_num);
+		CreateSockAddress(addr, NULL, port_num);
 	}
 
 	Address::Address(char *ip, int port_num)
 	{
-		CreateSockAddress(address, ip, port_num);
+		CreateSockAddress(addr, ip, port_num);
 	}
 
 	Address::~Address()
 	{
-		memset(&address, 0, sizeof(SockAddr));
+		memset(&addr, 0, sizeof(SockAddr));
 	}
 	
+	Message::Message()
+	{
+		memset(buffer, 0, DN_NETWORK_BUFFER_LENGTH);
+	}
+
+	Message::Message(const char* _data)
+	{
+		memset(buffer, 0, DN_NETWORK_BUFFER_LENGTH);		
+		strcpy_s(buffer, DN_NETWORK_BUFFER_LENGTH, _data);
+	}
+
+	Message::~Message()
+	{
+		memset(buffer, 0, DN_NETWORK_BUFFER_LENGTH);
+	}
+
+	Packet::Packet()
+	{}
+
+	Packet::Packet(char* msg, char* ip, int port_num) : msg(msg), address(ip, port_num)
+	{}
+
+	Packet::Packet(char* msg, Address addr) : msg(msg), address(addr)
+	{}
+
 }
--- DarkNet/DarkNetStructs.h
+++ DarkNet/DarkNetStructs.h
@@ -1,6 +1,9 @@
 #ifndef DARK_NET_STRUCTS
 #define DARK_NET_STRUCTS
 
+#include "DarkNetDefs.h"
+#include "DarkNetFuncs.h"
+
 namespace DarkNet
 {
 	struct UDPSocket
@@ -12,10 +15,37 @@ namespace DarkNet
 
 	struct Address
 	{
-		SockAddr address;
+		SockAddr addr;
+		Address();
 		Address(int port_num);
 		Address(char *ip, int port_num);
 		~Address();
+	};
+
+	struct Message
+	{
+		char buffer[DN_NETWORK_BUFFER_LENGTH];
+		size_t size()		{ return strlen(buffer);			}
+		size_t capacity()	{ return DN_NETWORK_BUFFER_LENGTH;	}
+		Message();
+		Message(const char* _data);
+		~Message();
+	};
+
+	struct Packet 
+	{
+		Message msg;
+		Address address;
+		Packet();
+		Packet(char* msg, char* ip, int port_num);
+		Packet(char* msg, Address addr);
+		Packet(Message msg, Address addr);
+	};
+
+	struct Peer
+	{
+		Packet	pck;
+		void*	pTag;
 	};
 }
 
